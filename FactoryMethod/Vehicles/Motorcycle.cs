0a7b3e8f5ca0af1126b749f9c1a811fd39899374
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2024-04-01 17:50:17 -0300

    Factory Method - Implementação de uma "fábrica"

---
+++ FactoryMethod/Vehicles/Motorcycle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FactoryMethod.Vehicles
+{
+    class Motorcycle : IVehicle
+    {
+        public void GetCargo()
+        {
+            Console.WriteLine("Pegamos a encomenda com a Moto!");
+        }
+
+        public void StartRoute()
+        {
+            GetCargo();
+            Console.WriteLine("Iniciando a entrega!");
+        }
+    }
+}
