29306b5f447032ec82e8b058d131104aecc82f1f
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2024-04-28 19:40:26 -0300

    Bridge

---
+++ Bridge/Plataforms/Facebook.cs
@@ -0,0 +1,22 @@
+﻿namespace Bridge.Plataforms
+{
+    public class Facebook : IPlatform
+    {
+        public Facebook()
+        {
+            ConfigureRMTP();
+            Console.WriteLine("Facebook: Transmissão Iniciada");
+        }
+
+        public void AuthToken()
+        {
+            Console.WriteLine("Facebook: Autorizando aplicação");
+        }
+
+        public void ConfigureRMTP()
+        {
+            AuthToken();
+            Console.WriteLine("Facebook: Configurando servidor RMTP");
+        }
+    }
+}
---
+++ Bridge/Program.cs
@@ -0,0 +1,41 @@
+﻿using Bridge.Plataforms;
+using Bridge.Transmissions;
+
+namespace Bridge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StartAdvancedLive(new YouTube());
+            StartLive(new Facebook());
+            StartLive(new TwitchTV());
+            StartAdvancedLive(new Instagram());
+
+            Console.ReadLine();
+        }
+
+        static void StartLive(IPlatform platform)
+        {
+            Console.WriteLine("Aguarde...");
+
+            Live live = new(platform);
+
+            live.Broadcasting();
+            live.Result();
+        }
+
+        static void StartAdvancedLive(IPlatform platform)
+        {
+            Console.WriteLine("Aguarde...");
+
+            AdvancedLive live = new(platform);
+
+            live.Broadcasting();
+            live.Subtitles();
+            live.Comments();
+            live.Reccords();
+            live.Result();
+        }
+    }
+}
