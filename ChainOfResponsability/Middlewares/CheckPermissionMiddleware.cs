691e056d348bec8e8dd0c5ac39b67ebf64805fbb
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2024-04-28 21:13:47 -0300

    Chain of Responsability

---
+++ ChainOfResponsability/Middlewares/CheckPermissionMiddleware.cs
@@ -0,0 +1,11 @@
+﻿namespace ChainOfResponsability.Middlewares
+{
+    public class CheckPermissionMiddleware : Middleware
+    {
+        public override bool Check(string email, string password)
+        {
+            Console.WriteLine("Seja bem-vindo!");
+            return CheckNext(email, password);
+        }
+    }
+}
---
+++ ChainOfResponsability/Program.cs
@@ -0,0 +1,46 @@
+﻿using ChainOfResponsability.Middlewares;
+using ChainOfResponsability.Servers;
+
+namespace ChainOfResponsability
+{
+    public class Program
+    {
+        private static Server server;
+
+        static void Init()
+        {
+            server = new Server();
+            server.RegisterUser("master@test.com", "123Abc");
+            server.RegisterUser("user@test.com", "123456");
+
+            Middleware middleware = new CheckUserMiddleware(server);
+
+            var checkPermissionMiddleware = new CheckPermissionMiddleware();
+
+            middleware.LinkWith(checkPermissionMiddleware);
+
+            server.SetMiddleware(middleware);
+        }
+
+        private static void Main(string[] args)
+        {
+            Init();
+
+            Boolean done = false;
+            do
+            {
+                Console.WriteLine("Digite o seu email: ");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Digite a sua senha: ");
+                string password = Console.ReadLine();
+
+                done = server.Login(email, password);
+
+            }
+            while (!done);
+
+            Console.ReadLine();
+        }
+    }
+}
